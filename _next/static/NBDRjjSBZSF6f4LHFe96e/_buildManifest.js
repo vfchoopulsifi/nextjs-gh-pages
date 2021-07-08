b870ea13aa24a34fae471b22bdbf91487e7f063a
gregrickaby <gregrickaby@users.noreply.github.com>
2021-07-08 02:30:43 +0000

    deploy: 37d00866420d1fbebad75bd0ddabb08516d9103d

--- _next/static/NBDRjjSBZSF6f4LHFe96e/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/ea6f1f42cebc7b8cbe53.css","static/chunks/pages/index-3e88a5d7e152c9f61e1b.js"],"/_error":["static/chunks/pages/_error-b30902e13465df7c5366.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
